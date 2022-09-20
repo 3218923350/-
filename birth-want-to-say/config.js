6c15063911f60cc3e27334de39e0a72464e194be
3218923350 <105201755+3218923350@users.noreply.github.com>
2022-09-20 17:54:04 +0800

    Add files via upload

---
+++ birth-want-to-say/config.js
@@ -0,0 +1,53 @@
+var config = {
+    texts: [
+        "送给我",      
+        "心爱的陈姑娘",  
+        "今天是你的生日",
+        "这是我们在一起的",
+        "第二个生日！",
+        "这一年来，我见过你",
+        "奇怪的样子",
+        "可爱的样子",
+        "生气的样子",
+        "都是我喜欢的样子",
+        "对了，礼物开学再给你哦",
+        "今年要吃好好的",
+        "喂饱饱的",
+        "然后才有力气干活",
+        "和想我！",
+        "生日快乐~~",
+    ],
+   
+    imgs: {
+        "送给我": "./imgs/songxinxin.jpeg",
+        "心爱的陈姑娘": "./imgs/1.jpg",
+        "今天是你的生日": "./imgs/shengri.jpeg",
+        "这是我们在一起的": "./zaiyiqi.jpg",
+        "第二个生日！": "./imgs/2.jpg",
+        "这一年来，我见过你": "./imgs/yangzi.jpeg",
+        "奇怪的样子": "./imgs/3.jpg",
+        "帅气的样子": "./imgs/4.jpg",
+        "生气的样子": "./imgs/5.jpg",
+        "都是我喜欢的样子": "./imgs/6.jpg",
+        "对了，礼物开学再给你哦": "./imgs/chuo.gif",
+        "今年要吃好好的": "./imgs/chihaodian.jpeg",
+        "喂饱饱的": "./imgs/7.jpg",
+        "然后才有力气干活": "./imgs/8.jpg",
+        "和想我！": "./imgs/9.jpg",
+        "生日快乐~~": "./imgs/10.jpg"
+    },
+    
+    desc: {
+        turn_on: "点我开始",
+        play: "上音乐",
+        bannar_coming: "搞点颜色",
+        balloons_flying: "好像还少点东西",
+        cake_fadein: "蛋糕？",
+        light_candle: "蜡烛？",
+        wish_message: "生日快乐～",
+        story: "A MESSAGE FOR YOU"
+    },
+
+
+    loveText: '爱你的王先生~'
+};
